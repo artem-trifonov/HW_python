f2cd78708a2ec92251633084199f105e0eeb7bae
artem-trifonov <53227596+artem-trifonov@users.noreply.github.com>
2020-09-25 09:25:50 +0300

    Update task3_huge_files.py
    Add independent path from the OS.
    Remove service characters in the files

--- task3_huge_files.py
+++ task3_huge_files.py
@@ -1,12 +1,13 @@
 from threading import Thread
 import time
+import os
 
 
 def file_one(file):
     while True:
         with open(file) as f:
             for line in f:
-                print(line, end='')
+                print(line.strip())
                 time.sleep(1)
 
 
@@ -14,7 +15,7 @@ def file_two(file):
     while True:
         with open(file) as f:
             for line in f:
-                print(line, end='')
+                print(line.strip())
                 time.sleep(1)
 
 
@@ -22,14 +23,14 @@ def file_three(file):
     while True:
         with open(file) as f:
             for line in f:
-                print(line, end='')
+                print(line.strip())
                 time.sleep(1)
 
 
 if __name__ == "__main__":
-    f1_thread = Thread(target=file_one, args=('resources/file1.txt',))
-    f2_thread = Thread(target=file_two, args=('resources/file2.txt',))
-    f3_thread = Thread(target=file_three, args=('resources/file3.txt',))
+    f1_thread = Thread(target=file_one, args=(os.path.join('recources', 'file1.txt'),))
+    f2_thread = Thread(target=file_two, args=(os.path.join('recources', 'file2.txt'),))
+    f3_thread = Thread(target=file_three, args=(os.path.join('recources', 'file3.txt'),))
 
     f1_thread.start()
     time.sleep(0.2)
