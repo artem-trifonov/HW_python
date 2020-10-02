c588bdb64cc81624f173d2874735f722f8576ef0
artem-trifonov <53227596+artem-trifonov@users.noreply.github.com>
2020-10-02 10:49:01 +0300

    Update task3_normal_files.py

--- task3_normal_files.py
+++ task3_normal_files.py
@@ -13,32 +13,32 @@ def task3(file1, file2, file3):
 
         while True:
             if f1_length > 0:
-                print(lines1[i], end='')
+                print(lines1[i].strip())
                 i += 1
                 f1_length -= 1
             else:
                 i = 0
-                print(lines1[i], end='')
+                print(lines1[i].strip())
                 i += 1
                 f1_length = len(lines1) - 1
 
             if f2_length > 0:
-                print(lines2[j], end='')
+                print(lines2[j].strip())
                 j += 1
                 f2_length -= 1
             else:
                 j = 0
-                print(lines2[j], end='')
+                print(lines2[j].strip())
                 j += 1
                 f2_length = len(lines2) - 1
 
             if f3_length > 0:
-                print(lines3[k], end='')
+                print(lines3[k].strip())
                 k += 1
                 f3_length -= 1
             else:
                 k = 0
-                print(lines3[k], end='')
+                print(lines3[k].strip())
                 k += 1
                 f3_length = len(lines3) - 1
 
